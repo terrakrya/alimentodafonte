9ae265b0b66516125ff9e293c596e4f721e5648c
Diego M. Rodrigues <diegomr86@gmail.com>
2019-04-09 22:33:50 -0300

    Build for deploy

--- src/views/CollectorsRequestForm.vue
+++ src/views/CollectorsRequestForm.vue
@@ -20,7 +20,7 @@
 					</div>		
 					<div class="row">
 						<div class="col-sm-12">
-							<form-seeds-select :form="form" field="field_paragraph_seeds" fieldtype="collectors_seeds_requests" :parent="this.$route.params.id" fieldseed="field_paragraph_seed" fieldextra="field_paragraph_weight" :seeds="seeds" v-if="seeds.length" />
+							<form-seeds-select :form="form" field="field_paragraph_seeds" fieldtype="collectors_seeds_requests" :parent="this.$route.params.id" fieldseed="field_paragraph_seed" fieldextra="field_paragraph_weight" :seeds="seeds" v-if="seeds.length" :callback="seedsChanged" />
 						</div>
 					</div>								
 					<form-submit :error="error" :sending="sending" />
@@ -49,6 +49,7 @@ export default {
 			error: false,
 			loading: false,
 			sending: false,
+			seeds_list: [],
 			form: {
 				type: [{ target_id: "requests_for_collectors" }],
 				title: [{ value: 'pedido-' + Date.now() }],
@@ -64,6 +65,7 @@ export default {
 		this.getList('collectors')
 		this.getList('collectors_groups')
 		this.getList('seeds_houses')
+		this.getList('potential_lists')
 		this.getList('seeds')
  
 		if (this.isEditing()) {
@@ -82,6 +84,9 @@ export default {
     },
     seeds () {
       return this.$store.state.seeds
+    },
+    potential_lists () {
+      return this.$store.state.potential_lists
     }
 	},
 	methods: {
@@ -112,8 +117,47 @@ export default {
 					}).catch(error => { this.error = error.response.data.message; this.sending = false })
 				}
 			})
+		}, 
+		seedsChanged (items) {
+			this.seeds_list = items
+		},
+		validateQty () {
+			this.qty_error = ''
+			if ((this.form.field_requests_group.length || this.form.field_requests_collector.length) && this.form.field_paragraph_seeds.length && this.potential_lists) {
+				let potential_list = this.potential_lists.find(pl => {
+					
+					let collector = this.present(this.form.field_requests_collector, 'target_id') ? this.form.field_requests_collector[0].target_id : null
+
+					let group = this.present(this.form.field_requests_group, 'target_id') ? this.form.field_requests_group[0].target_id : null
+					return (
+						(
+							(collector && pl.collector && pl.collector.id == collector) ||
+							(group && pl.group && pl.group.id == group)
+						) &&
+						pl.seeds && pl.seeds.find(s => (s.id == this.form.field_paragraph_seeds[0].target_id))
+					)
+				})
+
+				if (potential_list) {
+					let seed = potential_list.seeds.find(s => (s.id == this.form.field_paragraph_seeds[0].target_id))
+					if (Number(seed.weight) < Number(this.form.field_qty[0].value)) {
+						this.qty_error = 'Quantidade maior que a solicitada no Pedido '+potential_list.id+': '+ seed.weight + ' kg de '+seed.title
+						return false
+					} else {
+						return true
+					}
+				} else {
+					this.qty_error = 'Não existe registro de pedido dessa semente para este coletor/grupo'
+					return false
+				}
+			}
 		}
-	},
+	},	
+	// watch: {
+	// 	'form.field_paragraph_seeds': function () {
+	// 		this.validateQty()
+	// 	}
+	// },
 	components: { 
 		Breadcrumb, 
 		Loading, 
