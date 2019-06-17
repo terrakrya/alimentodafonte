e5f827baacba194633373e50030b6765b2838907
Diego M. Rodrigues <diegomr86@gmail.com>
2019-06-17 20:04:16 -0300

    Add docker file

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(require('method-override')());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 
