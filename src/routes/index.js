c7a4c742deaf0b47d8e051b13d0ff744dee0f0a3
Diego M. Rodrigues <diegomr86@gmail.com>
2019-11-24 17:34:13 -0300

    Certbot challenge

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,8 @@ var router = require('express').Router();
 
 router.use('/api', require('./api'));
 
+router.get('/.well-known/acme-challenge/343fzCmsCO3gOFr2HVxRwtbQLxliZLEpsgiyY3p-oIc', function(req, res) {
+  res.send('343fzCmsCO3gOFr2HVxRwtbQLxliZLEpsgiyY3p-oIc.0XMFlq6Y8YnP-dipKWnStwpqYcDSozaJeVDIUUqOAV0');
+});
+
 module.exports = router;
